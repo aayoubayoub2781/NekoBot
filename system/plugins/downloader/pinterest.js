e51d9d1100cefd82aa5727dacde9a894f9816f47
aayoubayoub2781 <148594510+aayoubayoub2781@users.noreply.github.com>
2025-03-08 23:04:45 +0000

    Update pinterest.js

--- system/plugins/downloader/pinterest.js
+++ system/plugins/downloader/pinterest.js
@@ -1,20 +1,20 @@
 let neko = async (m, { sock, Func, Scraper, text, Uploader }) => {
-  if (!text) throw "> *❌ Masukkan query atau link dari Pinterest!*";
+  if (!text) throw "> *❌ Enter a query or Pinterest link!*";
 
   if (Func.isUrl(text)) {
     if (!/pinterest.com|pin.it/.test(text))
-      throw "> *❌ Masukkan link Pinterest yang valid!*";
+      throw "> *❌ Enter a valid Pinterest link!*";
     let data = await Scraper.pinterest.download(text);
     let cap = "*– 乂 Pinterest - Downloader 📌*\n";
-    cap += `> *🔹 Judul :* ${data.title}\n`;
-    cap += `> *🔹 Kata Kunci :* ${data.keyword.join(", ")}\n`;
-    cap += `> *🔹 Pengarang :* ${data.author.name}\n`;
+    cap += `> *🔹 Title :* ${data.title}\n`;
+    cap += `> *🔹 Keywords :* ${data.keyword.join(", ")}\n`;
+    cap += `> *🔹 Author :* ${data.author.name}\n`;
 
     sock.sendFile(m.cht, data.download, null, cap, m);
   } else {
     let data = await Scraper.pinterest.search(text);
     let result = data.getRandom();
-    let caption = "*– 乂 Pinterest - Pencarian 🔍*\n";
+    let caption = "*– 乂 Pinterest - Search 🔍*\n";
     caption += Object.entries(result)
       .map(([a, b]) => `> *🔹 ${a.capitalize()} :* ${b}`)
       .join("\n");
@@ -34,7 +34,7 @@ neko.category = ["downloader", "tools"];
 neko.settings = {
   limit: true,
 };
-neko.description = "🔎 Mencari atau mengunduh media dari Pinterest!";
+neko.description = "🔎 Search or download media from Pinterest!";
 neko.loading = true;
 
 module.exports = neko;
